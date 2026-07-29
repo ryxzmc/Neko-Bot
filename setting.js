d0e25833fa4d8decb0c0ebd253df257e7853c12c
RyxzMC24XShiroko <ryxzmc24@gmail.com>
2026-07-29 23:32:59 +0800

    Update global settings and owner information

--- setting.js
+++ setting.js
@@ -6,19 +6,20 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 
 /*
-	* Create By Ryxz
+	* Create By RyxzDesu
 	* 
 	* 
 */
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ["62xxxxxxx"]  // Gunakan nomor WhatsApp kalian
-global.author = 'Ryxz'
+global.owner = ["6283141292575"]  // Gunakan nomor WhatsApp kalian
+global.author = 'RyxzDesu'
 global.botname = 'Neko-Bot'
-global.packname = 'Bot-Md'
-global.timezone = ''
-global.locale = ''
+global.number_bot = '6283141292575'  // Nomor bot tanpa +
+global.packname = 'Bot-MD'
+global.timezone = 'Asia/Makassar'
+global.locale = 'id-ID'
 global.listprefix = ["+","!","."]
 
 import { fileURLToPath } from 'url'
@@ -49,24 +50,24 @@ export default {
 }'
 
 
-global.my = {
-	yt: "https://",
+global.Owner = {
+	yt: "https://youtube.com/@RyxzOfficial",
 	web: 'https://ryxzmc.vercel.app
 	tele: 't.me/RyxzMC',
 	email: "ryxzmd24@gmail.com"
-	
-// TAMBAHIN SENDIRI DI SINI	
-global.webname = ''
-global.ig = ''
-global.email = ''
-global.sc = ''
-global.yt = ''
+
+global.webname = 'Neko-Bot Official' // isi brand botnya / website 
+global.website = 'Neko-Bot Official' // isi link web kalo udh bikin. Kalo belum Biarin kosong dulu
+global.ig = 'https://instagram.com/shirokodesu.store' // isi nama toko. jika belum ada kosongin aja
+global.email = 'ryxzmd24@gmail.com' // ketik email lu
+global.sc = 'https://github.com/ryxzxcode/Neko-Bot' // LINK GITHUB
+global.yt = 'https://youtube.com/@RyxzOfficial' // kalo ga ada ch yt kosongin aja
 
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
 global.tempatStore = 'baileys_store.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
 global.pairing_code = true
-global.number_bot = '' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
+global.number_bot = '62xxxxxxx' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
 
 global.fake = {
 	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
@@ -78,9 +79,9 @@ global.fake = {
 
 global.my = {
 	yt: "https://",
-	gh: "https://",
-	gc: "https://",
-	ch: ""
+	gh: "https://github.com/ryxzmc",
+	gc: "https://chat.whatsapp.com/LdZyZkGhldeGFhT0gV1Psv?s=cl&p=a&ilr=0",
+	ch: "https://whatsapp.com/channel/0029Vb8K2AJHgZWmrFZr5Z06"
 }
 
 global.limit = {
@@ -96,26 +97,26 @@ global.money = {
 }
 
 global.mess = {
-	key: "Apikey limit! Silahkan Upgrade: https://naze.biz.id",
+	key: "Apikey limit! Silahkan Upgrade: https://wa.me/6283141292575",
 	owner: "Khusus Owner!",
 	admin: "Khusus Admin!",
-	botAdmin: "Bot harus Admin!",
+	botAdmin: "Bot harus jadi Admin!",
 	onWa: "Nomor tersebut tidak terdaftar di WhatsApp!",
 	group: "Khusus Grup!",
 	private: "Khusus Private Chat!",
 	quoted: "Reply pesannya!",
-	limit: "Limit habis!",
+	limit: "❌ Limit apikey kamu habis. Reset jam 00:00 WIB",
 	prem: "Khusus Premium!",
 	text: "Masukkan teksnya!",
 	media: "Kirim medianya!",
-	wait: "Proses...",
+	wait: "Sedang di Proses...",
 	fail: "Gagal!",
 	error: "Error!",
 	done: "Selesai!"
 }
 
 global.APIs = {
-	ryxz: 'https://api.ryxz.dev.id',
+	Ryxzmc: 'https://api.ryxz.dev.id',
 	NekoBot: 'https://api.nekobot.xyz/6969',
 }
 global.APIKeys = {
@@ -123,18 +124,35 @@ global.APIKeys = {
 	'https://api.nekobot.xyz/6969': 'ROOT_NEKOBOT_ACCES_1337',
 }
 
-// Lainnya
-global.jadwalSholat = {
-	Subuh: '04:30',
-	Dzuhur: '12:06',
-	Ashar: '15:21',
-	Maghrib: '18:08',
-	Isya: '19:00'
+// ========== JADWAL ANIME & MANGA ==========
+global.jadwalRilis = {
+	// Format: Hari: [ {judul, jam, episode/chapter} ]
+	Senin: [
+		{ judul: 'One Piece', jam: '18:00', tipe: 'Manga Ch 1150' },
+		{ judul: 'Jujutsu Kaisen', jam: '20:00', tipe: 'Anime Eps 48' }
+	],
+	Selasa: [
+	{ judul: 'Spy x Family', jam: '19:00', tipe: 'Anime Eps 25' }
+	],
+	Rabu: [
+	    { judul: 'Chainsaw Man', jam: '21:00', tipe: 'Manga Ch 180' }
+	],
+	Kamis: [
+	{ judul: 'Demon Slayer', jam: '17:00', tipe: 'Anime Eps 55' }
+	],
+	Jumat: [
+	    { judul: 'Attack on Titan', jam: '22:00', tipe: 'Anime Eps 94' }
+	],
+	Sabtu: [
+	{ judul: 'Naruto', jam: '15:00', tipe: 'Anime Eps 500' }
+	],
+	Minggu: [
+	    { judul: 'My Hero Academia', jam: '16:30', tipe: 'Anime Eps 150' }
+	],
+	Senin: [
+	{ judul: 'Tensei shitara datta slime reincarnation', jam: '00:00', tipe: 'Anime Eps 16 }
+	]
 }
 
-global.badWords = ["dongo","konsol"] // input kata-kata toxic yg lain. ex: ['dongo','dongonya']
+global.badWords = ["bangsat","kontol","memek","anjing","pukimak kau"] 
 global.chatLength = 1000
-
-fs.watchFile(__filename, async () => {
-	console.log(chalk.yellowBright(`[UPDATE] ${__filename}`))
-});
