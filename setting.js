2a8d925ef404e8bb8fbe93c7f390fbc2c141f019
RyxzMC24XShiroko <ryxzmc24@gmail.com>
2026-09-19 20:35:07 +0800

    Refactor global settings and add new properties

--- setting.js
+++ setting.js
@@ -1,32 +1,38 @@
 import fs from 'fs';
 
-global.owner = ["6287894179931"]
+global.owner = [["https//wa.me/6283141292575","Ryxz",true]]
+global.mods = []
+global.pairingNumber = "6287894179931"
+
 global.botname = 'Neko-Bot'
 global.author = 'RyxzDesu'
-global.packname = 'Neko-Bot | © Created by RyxzXcode'
+global.packname = 'Neko-Bot_MD'
 global.number_bot = '6287894179931'
 
 global.webname = 'ShirokoDesu'
 global.website = 'https://ryxzmc.vercel.app'
 global.sc = 'https://github.com/ryxzmc/Neko-Bot'
 global.email = 'ryxzmd24@gmail.com'
-global.yt = 'https://youtube.com/@RyxzMC'
-global.ig = 'https://www.instagram.com/ryxz_mc24'
 
-global.pairing_code = true
 global.timezone = 'Asia/Makassar'
 global.tempatDB = 'database.json'
+global.tempatStore = 'baileys_store.json'
 
+global.fake = {
+  thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
+}
+
+global.my = {
+  gh: "https://github.com/ryxzmc",
+  ig: "https://www.instagram.com/ryxz_mc24",
+  tg: "t.me/RyxzMC",
+  ch: "https://whatsapp.com/channel/0029Vb8K2AJHgZWmrFZr5Z06"
+}
+
+global.limit = { free: 20, premium: 999, vip: 900 }
 global.mess = {
-  owner: "Maaf ya, ini khusus Owner😊!",
-  group: "Duhhh maaf kak, ini khusus grup!",
-  wait: "Tunggu ya,kami Sedang proses...",
-  done: "Selesai!"
-}
-
-global.APIs = {
-  Ryxzmc: 'https://api.ryxz.dev.id',
-}
-global.APIKeys = {
-  'https://api.ryxz.dev.id': 'nr-346227rnf61',
+  owner: "Maaf yaa🤧, ini Khusus Owner aja!",
+  group: "Maaf yaa☺️, ini Khusus Grup!",
+  wait: "Tunggu bentar, kami Sedang Proses...",
+  done: "Selesai✅!"
 }
